aa5731800f3d78dba3f9beb094b0eabb9c0a1209
Ana <anaiv9cs@yahoo.co.uk>
2022-01-08 21:38:38 +0200

    Dashboard activity and xml file created + finished design

---
+++ automotive/src/main/java/com.example.driverassistant/view/DashboardActivity.kt
@@ -0,0 +1,14 @@
+package com.example.driverassistant.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.driverassistant.R
+
+class DashboardActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dashboard_activity)
+    }
+
+}
