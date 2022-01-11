876e99a851ae0cba5f1087d97266aed23e5feb63
Alexandru Sava <alexandru.sava@brivo.com>
2022-01-12 00:46:29 +0200

    feat: Add data to improve driving skills and improve UI for this activity

--- automotive/src/main/java/com.example.driverassistant/view/ImproveSkillsActivity.kt
+++ automotive/src/main/java/com.example.driverassistant/view/ImproveSkillsActivity.kt
@@ -27,15 +27,28 @@ class ImproveSkillsActivity: AppCompatActivity() {
     }
 
     private fun initList(){
-        val a1 = AdviceModel("Ana", "descriere de test")
-        val a2 = AdviceModel("Ana", "descriere de test")
-        val a3 = AdviceModel("Ana", "descriere de test")
-        val a4 = AdviceModel("Ana", "descriere de test")
+        val a1 = AdviceModel(
+            "Respect Speed Limit",
+            "Did you know 90% of accidents happen because of speeding?"
+        )
+        val a2 = AdviceModel(
+            "Slow Down for Crosswalks",
+            "More than 70% of victims involved in accidents are pedestrians. " +
+                    "They are considered vulnerable road users."
+        )
+        val a3 = AdviceModel(
+            "Pay Attention to Cyclists",
+            "Cyclists may be unpredictable and they are also considered vulnerable road" +
+                    "users. Slow down when approaching one and leave them enough space."
+        )
+        val a4 = AdviceModel(
+            "Attention at Low Temperatures",
+            "Ice can form on the road and drivers sometimes do not realize it. You should" +
+                    " drive at least 25% slower than in normal conditions."
+        )
         val model: MutableList<AdviceModel> = mutableListOf(a1,a2,a3,a4)
 
-        listAdapter = AdviceAdaptor(model){
-            System.out.println("ceva")
-        }
+        listAdapter = AdviceAdaptor(model) { }
 
         recyclerView = findViewById(R.id.notificationRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
